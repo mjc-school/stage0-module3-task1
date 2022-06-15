e0e82496301470506bc9d674cd0188191088f6f9
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 15:42:31 +0300

    Fix task

--- src/main/java/lang/print/gaps/task1/BrokenApp.java
+++ src/main/java/lang/print/gaps/task1/BrokenApp.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task1;
 
 public class BrokenApp {
-    public void main(String args) {
+    public static void main(String args) {
         System.out,println("fixMe)
     }
 }
