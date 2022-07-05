9dc33861737d9ef26b2fb2ae09b6caf57c7dfcec
Aliaksandr Artsikhovich <aliaksandr_artsikhovich@epam.com>
2022-07-05 15:31:52 +0300

    delete existing solution, matched the description

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,8 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java");
-        System.out.println("is");
-        System.out.println("smth");
+        //write your code here
     }
 }
