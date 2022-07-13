af7ff512aa8eaa1f2d7fb3b59804c4af3976c5a0
Nurbek Abduraximov <84277066+Nurbek21@users.noreply.github.com>
2022-07-13 15:37:01 +0500

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,8 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java" + \n "is" + \n +"smth");
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
     }
 }
