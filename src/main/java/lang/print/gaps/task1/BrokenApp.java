a129e74706b378ad852fa6593596d15d96c9eab5
Olloyor <91249587+olloyor2000@users.noreply.github.com>
2022-10-06 09:40:27 +0500

    Update BrokenApp.java

--- src/main/java/lang/print/gaps/task1/BrokenApp.java
+++ src/main/java/lang/print/gaps/task1/BrokenApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class BrokenApp {
     public static void main(String args) {
-        System.out,println("fixMe)
+        System.out.println("fixMe");
     }
 }
