fb3436213773822ac63471c455df9da9f3b427a2
Olloyor <91249587+olloyor2000@users.noreply.github.com>
2022-10-06 09:43:01 +0500

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,5 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
+        System.out.println("This is my first Java program");
     }
 }
