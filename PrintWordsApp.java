f3b6152d02a5cab60108d05be04c6d80ecb20936
diyore008 <115913220+diyore008@users.noreply.github.com>
2022-10-17 12:24:12 +0500

    PrintWordsApp.java

---
+++ PrintWordsApp.java
@@ -0,0 +1,7 @@
+package lang.print.gaps.task1;
+
+public class PrintWordsApp {
+    public static void main(String[] args) {
+        System.out.println( "java" + "\n" + "is" + "\n" +"smth");
+    }
+}
