1348f37c4b58257dc523f5b3f87eeb6dafa1b4ea
diyore008 <115913220+diyore008@users.noreply.github.com>
2022-10-17 12:23:31 +0500

    PrintSentenceApp.java

---
+++ PrintSentenceApp.java
@@ -0,0 +1,7 @@
+package lang.print.gaps.task1;
+
+public class PrintSentenceApp {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java program");
+    }
+}
