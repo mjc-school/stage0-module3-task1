1d17182ada8ca0c614f6fa80a5ecb92557d0af3a
Lazizbek <81162929+Lazizbek-code@users.noreply.github.com>
2022-10-24 14:24:31 +0500

    fix:

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java" + " is " + "smth");
+        System.out.println("java" + "\n" + "is" + "\n" +"smth");
     }
 }
