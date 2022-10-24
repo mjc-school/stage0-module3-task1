6b650eb4f9371546a57f30a0e108461752be40d2
Lazizbek <81162929+Lazizbek-code@users.noreply.github.com>
2022-10-24 13:36:52 +0500

    Suntax

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.println("I'm updated can be compiled. And can write to console");
     }
 }
