bc6d33ab57430c1271e9a1e1ab704c79233cf877
Nik-kit <nikon12344@mail.com>
2023-03-18 13:40:36 +0400

    My second program is finished

--- src/main/java/lang/print/gaps/task1/FillGapsApp.java
+++ src/main/java/lang/print/gaps/task1/FillGapsApp.java
@@ -1,7 +1,8 @@
 package lang.print.gaps.task1;
 
 public class FillGapsApp {
-    public static void main(String[] args) {
-              .        ("Fixed");
+    public static void main(String[] args)
+    {
+        System.out.println("Fixed");
     }
 }
--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
+        System.out.println("java\nis\nsmth");
     }
 }
